ea536559f8e28acd7a02883b9a67beafc35c9fb4
Gideon Goldberg <gidsgoldberg@gmail.com>
2017-12-20 10:48:27 +0000

    wip add stub

--- spec/jobs/populate_register_data_in_db_job_spec.rb
+++ spec/jobs/populate_register_data_in_db_job_spec.rb
@@ -1,33 +1,32 @@
 require 'rails_helper'
 
 RSpec.configure do |config|
-
-config.before(:suite) do
-  DatabaseCleaner.strategy = :transaction
-  DatabaseCleaner.clean_with(:truncation)
-end
-
-config.around(:each) do |example|
-  DatabaseCleaner.cleaning do
-    example.run
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
   end
-end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
 end
 
 RSpec.describe PopulateRegisterDataInDbJob, type: :job do
-  # country_data = File.read('./spec/support/country.rsf')
-  # stub_request(:get, 'https://country.beta.openregister.org/download-rsf')
-  # .with(headers: { 'Accept' => '*/*', 'Accept-Encoding' => 'gzip, deflate', 'Host' => 'country.beta.openregister.org' })
-  # .to_return(status: 200, body: country_data, headers: {})
+  before(:each) do
+    ObjectsFactory.new.create_register('country', 'beta', 'Ministry of Justice')
+    country_data = File.read('./spec/support/country.rsf')
+    stub_request(:get, "https://country.beta.openregister.org/download-rsf").
+    with(headers: {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip, deflate', 'Host'=>'country.beta.openregister.org', 'User-Agent'=>'rest-client/2.0.2 (darwin15.6.0 x86_64) ruby/2.4.2p198'}).
+    to_return(status: 200, body: country_data, headers: {})
+  end
 
-
-
-  ObjectsFactory.new.create_register('country', 'beta', 'Ministry of Justice')
-
-  puts('in here')
-  puts(Spina::Register.count)
-  DatabaseCleaner.start
-  PopulateRegisterDataInDbJob.perform_now
-  DatabaseCleaner.clean
+  describe 'populate register data job' do
+    it 'populates data' do
+      puts('in here')
+      puts(Spina::Register.count)
+      PopulateRegisterDataInDbJob.perform_now
+    end
+  end
 end
-
