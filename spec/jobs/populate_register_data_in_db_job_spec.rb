b17199fdf2d94a57b602fe7e2958cb6500ab0eb6
Gideon Goldberg <gidsgoldberg@gmail.com>
2017-12-21 17:50:29 +0000

    add tests

--- spec/jobs/populate_register_data_in_db_job_spec.rb
+++ spec/jobs/populate_register_data_in_db_job_spec.rb
@@ -15,7 +15,6 @@ end
 
 RSpec.describe PopulateRegisterDataInDbJob, type: :job do
   before(:each) do
-    ObjectsFactory.new.create_register('country', 'beta', 'Ministry of Justice')
     country_data = File.read('./spec/support/country.rsf')
     stub_request(:get, "https://country.beta.openregister.org/download-rsf/0").
     with(headers: { 'Accept'=>'*/*', 'Accept-Encoding'=>'gzip, deflate', 'Host'=>'country.beta.openregister.org' }).
@@ -25,14 +24,21 @@ RSpec.describe PopulateRegisterDataInDbJob, type: :job do
     stub_request(:get, "https://country.beta.openregister.org/download-rsf/207").
     with(headers: { 'Accept'=>'*/*', 'Accept-Encoding'=>'gzip, deflate', 'Host'=>'country.beta.openregister.org' }).
     to_return(status: 200, body: country_update, headers: {})
+
+    ObjectsFactory.new.create_register('country', 'beta', 'Ministry of Justice')
+    PopulateRegisterDataInDbJob.perform_now
   end
 
   describe 'populate register data job' do
-    it 'populates data' do
+    it 'incrementally updates data' do
       expect(Spina::Register.count).to eq(1)
-      PopulateRegisterDataInDbJob.perform_now
-      expect(Record.where(spina_register_id: Spina::Register.find_by(name: 'country').id).count).to eq(208)
-      PopulateRegisterDataInDbJob.perform_now
+      expect(Entry.where(spina_register_id: Spina::Register.find_by(name: 'country').id).count).to eq(220)
+      expect(Record.find_by(key: 'CI').data['citizen-names']).to eq('Citizen of the Ivory Coast EDIT')
+      expect(Entry.where(key: 'CI').last.data['citizen-names']).to eq('Citizen of the Ivory Coast EDIT')
+    end
+
+    it 'retains existing entries' do
+      expect(Entry.where(key: 'CI').first.data['citizen-names']).to eq('Citizen of the Ivory Coast')
     end
   end
 end
